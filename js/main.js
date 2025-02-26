0275aabd8f35a64f4c6617562f7013d7dd9eec21
2Estella <krsd0501@naver.com>
2025-02-27 01:07:45 +0900

    ⚡️ : 불필요한 스크립트, 스타일 제거 및 폰트 최적화
    - country-bar 관련 불필요한 스크립트, 스타일 제거
    - 웹 폰트 중 사용하지 않는 font-weight 제거, 대체 폰트 추가

--- js/main.js
+++ js/main.js
@@ -4,12 +4,8 @@ function showTopBar(){
     setTimeout(() => {
         document.querySelector("section.country-bar")
         .innerHTML = `<p>Orders to <b>${country}</b> are subject to <b>${vat}%</b> VAT</p>`
-        document.querySelector("section.country-bar")
-        .classList.remove('hidden')
     }, 1000);
 }
-
-
 
 showTopBar();
 
